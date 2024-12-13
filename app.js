19b494203172c86a6f8401941d8afb48d50ee297
Naftannafi <s121921samisha@gmail.com>
2024-12-13 09:41:16 -0900

    testing server connect with databse

--- app.js
+++ app.js
@@ -10,12 +10,32 @@
 // myServer.listen(1234, () => {
 //   console.log("server is running");
 // });
+
+// app.use(express.static("./express/AppleResponsive"));
+// const PORT = 3030;
+// app.listen(PORT, (err) => {
+//   if (err) console.log(err);
+//   console.log("this the error message ");
+//   console.log(`server running on http://localhost:${PORT}`);
+// });
 const express = require("express");
 const app = express();
-app.use(express.static("./express/AppleResponsive"));
-const PORT = 3030;
-app.listen(PORT, (err) => {
-  if (err) console.log(err);
-  console.log("this the error message ");
-  console.log(`server running on http://localhost:${PORT}`);
+const mysql = require("mysql2");
+// app.get("/"(req,res)=>res.send("up and runnuing"));
+
+app.listen(3001, () => console.log("listening and running on ...."));
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "phptesting",
+  password: "s12@Naftann",
+  database: "phptesting",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting: " + err.stack);
+    return;
+  }
+  console.log("Connected as id " + connection.threadId);
 });
